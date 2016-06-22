9bda49579a754a3fa36effc7a243b208bc631e30
Neil McGill <goblinhack@gmail.com>
2016-06-22 08:11:05 -0400

    set and get calls
    Former-commit-id: fd78565faf52124f0f3f51b7f3c6c36873e10f02

--- data/config.py
+++ data/config.py
@@ -2,4 +2,8 @@ import mm
 
 def set_game_video_pix_width (value):
     mm.game_video_pix_width = value
+    mm.con(str(mm.game_video_pix_width))
+
+def get_game_video_pix_width (x):
+    return (mm.game_video_pix_width)
     
