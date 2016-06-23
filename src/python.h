b16211a594b92e37cf24316322c305ad619a828e
Neil McGill <goblinhack@gmail.com>
2016-06-22 23:18:31 -0400

    test pycall

--- src/python.h
+++ src/python.h
@@ -12,10 +12,12 @@ void python_init(void);
 void python_fini(void);
 void py_exec(const char *str);
 
-void py_call_vi(const char *name, int val1);
-void py_call_vmi(const char *module, const char *name, int val1);
-int py_call_ii(const char *name, int val1);
-int py_call_imi(const char *module, const char *name, int val1);
+void py_call_void_int(const char *name, int val1);
+int py_call_int_int(const char *name, int val1);
+
+void py_call_void_module_int(const char *module, const char *name, int val1);
+int py_call_int_module_int(const char *module, const char *name, int val1);
+int py_call_int_module_void(const char *module, const char *name);
 
 void py_err(void);
 char *py_obj_to_str(const PyObject *py_str);
