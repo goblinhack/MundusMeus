0f2e6bfd1cf6b575127be69cb53cdf5c6e78bebb
Neil McGill <goblinhack@gmail.com>
2016-06-26 15:49:59 -0400

    wip

--- data/world.py
+++ data/world.py
@@ -32,26 +32,29 @@ class World:
         #
         # Stack of level co-ordinates so we can backtrack out of dungeons
         #
-        self.levels = []
+        self.level_stack = []
 
         self.log("Created")
 
     def destroy (self):
+        self.log("Destroying world {")
+
         while self.level != None:
             self.pop_level()
 
-        self.log("destroyed")
+        self.log("} Destroyed world")
+
         del self
 
     def __str__(self):
         return "world:{0}".format(self.world_id)
 
     def log (self, msg):
-        print("{0: <20}: {1: <15}: WORLD: {2}".
+        print("{0: <19}: {1: <25}: WORLD: {2}".
                 format(Timestamp(), self, msg))
 
     def err (self, msg):
-        print("{0: <20}: {1: <15}: WORLD: ERROR: {2}".
+        print("{0: <19}: {1: <25}: WORLD: ERROR: {2}".
                 format(Timestamp(), self, msg))
         traceback.print_stack()
 
@@ -74,57 +77,80 @@ class World:
         return level
 
     def push_level (self, xyz):
+        self.log("Push level {0}".format(xyz))
+
         #
         # Add a new level onto the stack. Destroy the current level first.
         #
         if self.level != None:
+            self.level.save()
             self.level.destroy()
             self.level = None
 
-        self.levels.append(xyz)
+        self.level_stack.append(xyz)
         self.level = self.load_level(xyz)
+        self.save()
 
     def pop_level (self):
+        self.save()
+
         #
         # Destory the current level and try and load the next in the stack.
         #
-        if not len(self.levels):
+        if not len(self.level_stack):
             return
+
+        xyz = self.level_stack[len(self.level_stack)-1]
+        self.log("Pop level {0}".format(xyz))
 
         if self.level != None:
             self.level.destroy()
             self.level = None
             
-        self.levels.pop()
+        self.level_stack.pop()
 
-        if not len(self.levels):
+        if not len(self.level_stack):
             return
 
-        xyz = self.levels[len(self.levels)-1]
         self.level = self.load_level(xyz)
 
     def save (self):
-        self.log("save")
+        self.log("Save world")
         with open(str(self), 'wb') as f:
-            pickle.dump(self.world_id, f, pickle.HIGHEST_PROTOCOL)
-            pickle.dump(self.max_thing_id, f, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(self.world_id, f,           pickle.HIGHEST_PROTOCOL)
+            pickle.dump(self.max_thing_id, f,       pickle.HIGHEST_PROTOCOL)
+            pickle.dump(self.level_stack, f,        pickle.HIGHEST_PROTOCOL)
 
         self.level.save()
 
     def load (self):
-        self.log("load")
+        self.log("Load world")
         with open(str(self), 'rb') as f:
-            self.world_id = pickle.load(f)
-            self.max_thing_id = pickle.load(f)
+            self.world_id                           = pickle.load(f)
+            self.max_thing_id                       = pickle.load(f)
+            self.level_stack                        = pickle.load(f)
 
-        xyz = self.levels[len(self.levels)-1]
-
-        self.level = self.load_level(xyz)
+        #
+        # Now load the last level we looked at
+        #
+        if len(self.level_stack) == 0:
+            #
+            # Just load the default level
+            #
+            self.err("Empty level stack")
+            p = Xyz(0,0,0)
+            self.push_level(p)
+        else:
+            xyz = self.level_stack[len(self.level_stack)-1]
+            self.level = self.load_level(xyz)
 
     def dump (self):
-        self.log("{0: <20} {1}".format("world_id", self.world_id))
-        self.log("{0: <20} {1}".format("max_thing_id", self.max_thing_id))
-        self.log("{0: <20} {1}".format("levels", self.levels))
+        self.log("DUMP: {0: <19} {1}".format("world_id", 
+            self.world_id))
+        self.log("DUMP: {0: <19} {1}".format("max_thing_id", 
+            self.max_thing_id))
+        self.log("DUMP: {0: <19} {1}".format("level_stack", 
+            self.level_stack))
 
         if self.level != None:
             self.level.dump()
@@ -139,16 +165,18 @@ class Level:
         return "{0},level:{1}".format(self.world, self.xyz)
 
     def destroy (self):
+        self.log("Destroying level {")
         for key,value in self.all_things.items():
             value.destroy()
             self.all_things.pop(key)
-        self.log("destroyed")
+        self.log("} Destroyed level")
+        del self
 
     def log (self, msg):
-        print("{0: <20}: {1: <15}: LEVEL: {2}".format(Timestamp(), self, msg))
+        print("{0: <19}: {1: <25}: LEVEL: {2}".format(Timestamp(), self, msg))
 
     def err (self, msg):
-        print("{0: <20}: {1: <15}: LEVEL: ERROR: {2}".format(
+        print("{0: <19}: {1: <25}: LEVEL: ERROR: {2}".format(
             Timestamp(), self, msg))
         traceback.print_stack()
 
@@ -157,6 +185,8 @@ class Level:
             self.all_things[i].dump()
 
     def save (self):
+        self.log("Save level")
+
         with open(str(self), 'wb') as f:
             pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
 
@@ -179,7 +209,7 @@ class Thing:
         self.y = -1
         self.on_map = False
 
-        self.log("created")
+        self.log("Created thing")
 
         if self.thing_id in self.level.all_things:
             self.err("Already in level list")
@@ -188,7 +218,7 @@ class Thing:
         self.level.all_things[self.thing_id] = self
 
     def __str__(self):
-        return "{0}: id[{0}]:{1}".format(self.level, self.thing_id, self.name)
+        return "{0}: id[{1}]:{2}".format(self.level, self.thing_id, self.name)
 
     def destroy (self):
         if self.on_map:
@@ -197,14 +227,15 @@ class Thing:
         if self.thing_id in self.level.all_things:
             self.level.all_things[self.thing_id] = []
 
-        self.log("destroyed")
+        self.log("Destroyed thing")
         del self
 
     def log (self, msg):
-        print("{0: <20}: {1: <15}: {2}".format(Timestamp(), self, msg))
+        print("{0: <19}: {1: <25}: {2}".format(Timestamp(), self, msg))
 
     def err (self, msg):
-        print("{0: <20}: {1: <15}: ERROR: {2}".format(Timestamp(), self.debug, msg))
+        print("{0: <19}: {1: <25}: ERROR: {2}".format(
+            Timestamp(), self.debug, msg))
         traceback.print_stack()
 
     def dump (self):
@@ -229,27 +260,32 @@ class Thing:
         self.level.on_map[self.x][self.y].remove(self)
         self.on_map = False
 
+def save_test():
+    w = World(0)
+    
+    p = Xyz(0,0,0)
+    w.push_level(p)
+    l = w.get_level()
+    l.set_dim(256, 256)
+    
+    p2 = Xyz(0,0,1)
+    w.push_level(p2)
+    l = w.get_level()
+    l.set_dim(256, 256)
+    
+    for i in range(0,3):
+        t = Thing(level=l, name="grass")
+        t.push(10 + 1, 10 + 1)
+        t.pop()
 
-w = World(0)
+    w.save()
 
-p = Xyz(0,0,0)
-w.push_level(p)
-l = w.get_level()
-l.set_dim(256, 256)
+def load_test():
+    w = World(0)
+    w.load()
+    w.dump()
 
-p2 = Xyz(0,0,1)
-w.push_level(p2)
-l = w.get_level()
-l.set_dim(256, 256)
 
-for i in range(0,3):
-    t = Thing(level=l, name="grass")
-    t.push(10 + 1, 10 + 1)
-    t.pop()
-
-w.save()
-
-w.load()
-w.dump()
-w.destroy()
-
+save_test()
+print("===============")
+load_test()
