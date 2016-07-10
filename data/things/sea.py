4a5e8ea6d72e57dad3c7cdf900a4ee906aba59c2
Neil McGill <goblinhack@gmail.com>
2016-07-10 07:50:44 -0400

    sea

---
+++ data/things/sea.py
@@ -0,0 +1,76 @@
+from tp import tpp
+import mm
+
+def init():
+    x = tpp("sea1");
+    x.set_short_name("sea");
+    x.set_is_land(True);
+    x.set_is_sea(True);
+    x.set_world_depth("land");
+
+    mm.tp_set_tile(x, tile="sea1.1");
+    mm.tp_set_tile(x, tile="sea1.2");
+    mm.tp_set_tile(x, tile="sea1.3");
+    mm.tp_set_tile(x, tile="sea1.4");
+    mm.tp_set_tile(x, tile="sea1.5");
+    mm.tp_set_tile(x, tile="sea1.6");
+    mm.tp_set_tile(x, tile="sea1.7");
+    mm.tp_set_tile(x, tile="sea1.8");
+    mm.tp_set_tile(x, tile="sea1.9");
+    mm.tp_set_tile(x, tile="sea1.10");
+    mm.tp_set_tile(x, tile="sea1.11");
+    mm.tp_set_tile(x, tile="sea1.12");
+    mm.tp_set_tile(x, tile="sea1.13");
+    mm.tp_set_tile(x, tile="sea1.14");
+    mm.tp_set_tile(x, tile="sea1.15");
+    mm.tp_set_tile(x, tile="sea1.16");
+    mm.tp_set_tile(x, tile="sea1.17");
+    mm.tp_set_tile(x, tile="sea1.18");
+    mm.tp_set_tile(x, tile="sea1.19");
+    mm.tp_set_tile(x, tile="sea1.20");
+    mm.tp_set_tile(x, tile="sea1.21");
+    mm.tp_set_tile(x, tile="sea1.22");
+    mm.tp_set_tile(x, tile="sea1.23");
+    mm.tp_set_tile(x, tile="sea1.24");
+    mm.tp_set_tile(x, tile="sea1.25");
+    mm.tp_set_tile(x, tile="sea1.26");
+    mm.tp_set_tile(x, tile="sea1.27");
+    mm.tp_set_tile(x, tile="sea1.28");
+    mm.tp_set_tile(x, tile="sea1.29");
+    mm.tp_set_tile(x, tile="sea1.30");
+    mm.tp_set_tile(x, tile="sea1.31");
+    mm.tp_set_tile(x, tile="sea1.32");
+    mm.tp_set_tile(x, tile="sea1.33");
+    mm.tp_set_tile(x, tile="sea1.34");
+    mm.tp_set_tile(x, tile="sea1.35");
+    mm.tp_set_tile(x, tile="sea1.36");
+    mm.tp_set_tile(x, tile="sea1.37");
+    mm.tp_set_tile(x, tile="sea1.38");
+    mm.tp_set_tile(x, tile="sea1.39");
+    mm.tp_set_tile(x, tile="sea1.40");
+    mm.tp_set_tile(x, tile="sea1.41");
+    mm.tp_set_tile(x, tile="sea1.42");
+    mm.tp_set_tile(x, tile="sea1.43");
+    mm.tp_set_tile(x, tile="sea1.44");
+    mm.tp_set_tile(x, tile="sea1.45");
+    mm.tp_set_tile(x, tile="sea1.46");
+    mm.tp_set_tile(x, tile="sea1.47");
+    mm.tp_set_tile(x, tile="sea1.48");
+    mm.tp_set_tile(x, tile="sea1.49");
+    mm.tp_set_tile(x, tile="sea1.50");
+    mm.tp_set_tile(x, tile="sea1.51");
+    mm.tp_set_tile(x, tile="sea1.52");
+    mm.tp_set_tile(x, tile="sea1.53");
+    mm.tp_set_tile(x, tile="sea1.54");
+    mm.tp_set_tile(x, tile="sea1.55");
+    mm.tp_set_tile(x, tile="sea1.56");
+    mm.tp_set_tile(x, tile="sea1.57");
+    mm.tp_set_tile(x, tile="sea1.58");
+    mm.tp_set_tile(x, tile="sea1.59");
+    mm.tp_set_tile(x, tile="sea1.60");
+    mm.tp_set_tile(x, tile="sea1.61");
+    mm.tp_set_tile(x, tile="sea1.62");
+    mm.tp_set_tile(x, tile="sea1.63");
+    mm.tp_set_tile(x, tile="sea1.64");
+
+init()
