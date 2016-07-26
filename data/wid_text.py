3d07fb4d28ced1eea39ede4579b24fecd7a16744
Neil McGill <goblinhack@gmail.com>
2016-07-25 22:37:58 -0400

    some more text

--- data/wid_text.py
+++ data/wid_text.py
@@ -7,6 +7,7 @@ class WidText(wid.Wid):
 
     def __init__(self, name, 
                  x1, y1, x2, y2, 
+                 tiles,
                  text, 
                  font="small",
                  parent=0, 
@@ -14,7 +15,7 @@ class WidText(wid.Wid):
         self.parent = parent
         self.name = name
 
-        super().__init__(name, **kw)
+        super().__init__(name, tiles=tiles, parent=parent)
         self.set_tl_br_pct(x1, y1, x2, y2)
 
         self.log("Created wid")
@@ -23,22 +24,34 @@ class WidText(wid.Wid):
         self.width = x2 - x1
         self.height = y2 - y1
 
+        self.tile_size = mm.tile_size_pct(tiles + "-tl")
+        self.tile_width = self.tile_size[0]
+        self.tile_height = self.tile_size[1]
+
+        mm.con("{0}".format(self.tile_width))
+
+        self.usable_width = self.width - self.tile_width
+        self.usable_height = self.height - self.tile_height
+
         lines = text.split("\n")
-        y = 0
+        y = self.tile_height * 0.5
 
         for line in lines:
 
             words = line.split()
-            x = 0
+            x = self.tile_width * 0.5
 
             for word in words:
                 w, h = mm.text_size_pct(font=font, text=word + " ")
 
-                w = w / self.width
-                h = h / self.height
+                w = w / self.usable_width
+                h = h / self.usable_height
 
-                mm.con("{0} {1} {2} {3} {4}".format(word, x, y, w, h))
+                if x + w > 1.0 - self.tile_width * 0.5 :
+                    x = self.tile_width * 0.5
+                    y = y + h
 
+                print("{0} ---{1}---".format(x, word))
                 child = wid.Wid(name="wid text child", parent=self.wid_id)
                 child.set_tl_br_pct(x, y, x + w, y + h)
                 child.set_text(text=word, font=font)
---
+++ src/py_tile.c
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2011-2017 goblinhack@gmail.com
+ *
+ * See the LICENSE file for license.
+ */
+
+#include "python.h"
+#include "main.h"
+#include "string_util.h"
+#include "string_ext.h"
+#include "frameobject.h"
+#include "tex.h"
+#include "tile.h"
+#include "pixel.h"
+#include "thing_template.h"
+
+PyObject *tile_load_arr_ (PyObject *obj, PyObject *args, PyObject *keywds)
+{
+    char *tex_name = "unset tex_name";
+    char *tex_name_black_and_white = "unset tex_name_black_and_white";
+    int width = 0;
+    int height = 0;
+    PyObject *e = 0;
+
+    static char *kwlist[] = {"tex_name", "tex_name_black_and_white", "width", "height", "arr", 0};
+
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "ssiiO", kwlist, &tex_name, &tex_name_black_and_white, &width, &height, &e)) {
+        Py_RETURN_NONE;
+    }
+
+    if (!tex_name) {
+        ERR("tile_load_arr, missing tex_name attr");
+        Py_RETURN_NONE;
+    }
+
+    if (!tex_name_black_and_white) {
+        ERR("tile_load_arr, missing tex_name_black_and_white attr");
+        Py_RETURN_NONE;
+    }
+
+    if (!width) {
+        ERR("tile_load_arr, missing width attr");
+        Py_RETURN_NONE;
+    }
+
+    if (!height) {
+        ERR("tile_load_arr, missing height attr");
+        Py_RETURN_NONE;
+    }
+
+    if (!e) {
+        ERR("tile_load_arr, missing arr attr");
+        Py_RETURN_NONE;
+    }
+
+    int numLines = PyList_Size(e);
+    const char *arr[numLines + 1];
+    int i;
+
+    memset(arr, 0, sizeof(arr));
+
+    LOG("tile_load_arr(tex_name=%s, tex_name_black_and_white=%s, width=%d, height=%d, ...)", tex_name, tex_name_black_and_white, width, height);
+
+    for (i=0; i<numLines; i++){
+        PyObject * strObj;
+        strObj = PyList_GetItem(e, i); /* Can't fail */
+        if (!strObj) {
+            continue;
+        }
+
+        arr[i] = py_obj_to_str(strObj);
+    }
+
+    tile_load_arr(tex_name, tex_name_black_and_white, width, height, numLines, arr);
+
+    for (i=0; i<numLines; i++){
+        if (!arr[i]) {
+            continue;
+        }
+
+        myfree((char*) arr[i]);
+    }
+
+    Py_RETURN_NONE;
+}
+
+PyObject *tile_get_size_ (PyObject *obj, PyObject *args, PyObject *keywds)
+{
+    char *name = 0;
+    tilep tile = 0;
+
+    static char *kwlist[] = {
+        "name", 
+        0
+    };
+
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "s", kwlist, 
+                                     &name)) {
+        Py_RETURN_NONE;
+    }
+
+    if (!name) {
+        DIE("no tile name");
+    }
+
+    tile = tile_find(name);
+    if (!tile) {
+        DIE("no font named %s", name);
+    }
+
+    double w = tile_get_width(tile);
+    double h = tile_get_height(tile);
+
+    return (Py_BuildValue("dd", w, h));
+}
+
+PyObject *tile_get_size_pct_ (PyObject *obj, PyObject *args, PyObject *keywds)
+{
+    char *name = 0;
+    tilep tile = 0;
+
+    static char *kwlist[] = {
+        "name", 
+        0
+    };
+
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "s", kwlist, 
+                                     &name)) {
+        Py_RETURN_NONE;
+    }
+
+    if (!name) {
+        DIE("no tile name");
+    }
+
+    tile = tile_find(name);
+    if (!tile) {
+        DIE("no font named %s", name);
+    }
+
+    double w = tile_get_width(tile);
+    double h = tile_get_height(tile);
+
+    w = w / ((double) game.video_gl_width);
+    h = h / ((double) game.video_gl_height);
+
+    return (Py_BuildValue("dd", w, h));
+}
