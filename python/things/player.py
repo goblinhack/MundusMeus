d678ef6a4e365f112bfb67adb63737fdcf419313
Neil McGill <goblinhack@gmail.com>
2016-08-09 22:27:20 -0400

    thing init
    Former-commit-id: 07fbcbc25dc27be4003021519d95d71ab7c015c6

--- python/things/player.py
+++ python/things/player.py
@@ -1,17 +1,21 @@
 from tp import tpp
 import mm
 
+def thing_init(t):
+    t.set_description("A grumbly little creature of dark intent")
+#    x.add_damage("fists", hitdice=1, d=6)
+#    x.add_damage("bite", hitdice=1, d=6)
+    return
 
 def init():
     x = tpp("player1")
     x.set_short_name("player1")
-#    x.set_description("A grumbly little creature of dark intent")
-#    x.set_damage("fists", hitdice=1, d=6)
-#    x.set_damage("fists", hitdice=1, d=6)
     x.set_is_player(True)
     x.set_light_radius(6)
     x.set_world_depth("player")
 
     mm.tp_set_tile(x, tile="player1")
+
+    x.thing_init = thing_init
 
 init()
