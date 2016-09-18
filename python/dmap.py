9e254ce7b49cc9041f7c074c55b1d51c3fc2cf0d
Neil McGill <goblinhack@gmail.com>
2016-09-18 17:48:44 -0400

    dmap

---
+++ python/dmap.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+import sys
+import copy
+import random
+import os
+
+WALL = 10
+
+
+class Dmap:
+    def __init__(self, width=80, height=40):
+
+        self.width = width
+        self.height = height
+        self.cells = [[0 for i in range(self.height)]
+                      for j in range(self.width)]
+
+    def dump(self):
+        for y in range(self.height):
+            for x in range(self.width):
+
+                c = self.cells[x][y]
+
+                if c == WALL:
+                    c = " "
+                else:
+                    c = chr(ord('0') + int(c))
+
+                sys.stdout.write(c)
+            print("")
+
+    #
+    # An optimized djkstra map that only looks at map areas that change.
+    #
+    def process(self):
+
+        valid = [[1 for i in range(self.height)]
+                 for j in range(self.width)]
+        orig_valid = [[1 for i in range(self.height)]
+                      for j in range(self.width)]
+
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.cells[x][y] == WALL:
+                    valid[x][y] = 0
+                    orig_valid[x][y] = 0
+
+        new_cells = [[0 for i in range(self.height)]
+                     for j in range(self.width)]
+
+        changed = True
+        while changed is True:
+            changed = False
+            new_valid = [[0 for i in range(self.height)]
+                         for j in range(self.width)]
+
+            for y in range(1, self.height - 1):
+                for x in range(1, self.width - 1):
+
+                    new_cells[x][y] = self.cells[x][y]
+
+                    if orig_valid[x][y] == 0:
+                        continue
+
+                    if valid[x][y] == 0:
+                        continue
+
+                    a = self.cells[x-1][y-1] * 2
+                    b = self.cells[x][y-1]
+                    c = self.cells[x+1][y-1] * 2
+                    d = self.cells[x-1][y]
+                    e = self.cells[x][y]
+                    f = self.cells[x+1][y]
+                    g = self.cells[x-1][y+1] * 2
+                    h = self.cells[x][y+1]
+                    i = self.cells[x+1][y+1] * 2
+
+                    if a < b:
+                        lowest = a
+                    else:
+                        lowest = b
+
+                    if c < lowest:
+                        lowest = c
+
+                    if d < lowest:
+                        lowest = d
+
+                    if f < lowest:
+                        lowest = f
+
+                    if g < lowest:
+                        lowest = g
+
+                    if h < lowest:
+                        lowest = h
+
+                    if i < lowest:
+                        lowest = i
+
+                    if e - lowest >= 2:
+                        new_valid[x-1][y-1] = 1
+                        new_valid[x-1][y] = 1
+                        new_valid[x-1][y+1] = 1
+                        new_valid[x][y-1] = 1
+                        new_valid[x][y] = 1
+                        new_valid[x][y+1] = 1
+                        new_valid[x+1][y-1] = 1
+                        new_valid[x+1][y] = 1
+                        new_valid[x+1][y+1] = 1
+
+                        new_cells[x][y] = lowest + 1
+                        changed = True
+
+            valid = copy.deepcopy(new_valid)
+            self.cells = copy.deepcopy(new_cells)
+
+            os.system("clear")
+            self.dump()
+
+
+def main():
+
+    width = 64
+    height = 64
+
+    dmap = Dmap(width=width, height=height)
+
+    for y in range(height):
+        for x in range(width):
+            dmap.cells[x][y] = WALL - 1
+
+    for i in range(0, 100):
+        x = random.randint(0, width - 1)
+        y = random.randint(0, height - 1)
+        dmap.cells[x][y] = WALL
+
+    for i in range(0, 3):
+        border = 3
+        x = random.randint(border, width - border)
+        y = random.randint(border, height - border)
+        for dx in range(-border, border):
+            for dy in range(-border, border):
+                if random.randint(0, 100) < 50:
+                    dmap.cells[x + dx][y + dy] = 0
+
+    dmap.process()
+
+    os.system("clear")
+    dmap.dump()
+
+
+main()
