30292d2334c381d6f3fb164498abd11879d6ec3f
Neil McGill <goblinhack@gmail.com>
2016-10-04 08:13:06 -0400

    deco

---
+++ python/things/deco.py
@@ -0,0 +1,38 @@
+import tp
+
+
+def thing_init(t):
+    t.set_description("deco1")
+
+
+def deco1_init(name, short_name, tiles=[]):
+    x = tp.Tp(name)
+    x.set_short_name(short_name)
+    x.set_z_depth("player")
+    x.set_blit_top_off(1)
+
+    if tiles is not None:
+        for t in tiles:
+            x.set_tile(t, delay_ms=50)
+    else:
+        x.set_tile(tile=name, delay_ms=50)
+
+    x.thing_init = thing_init
+
+    return x
+
+
+def init():
+    deco1_init(name="deco1",         short_name="deco1",
+               tiles=[
+                      "deco1.1",
+                      "deco1.2",
+                      "deco1.3",
+                      "deco1.4",
+                      "deco1.5",
+                      "deco1.6",
+                      "deco1.7",
+                      "deco1.8",
+                     ])
+
+init()
