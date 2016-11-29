2cf6a23150f53cc059fb7397caf50754de61de64
Neil McGill <goblinhack@gmail.com>
2016-11-28 20:55:03 -0500

    dungeon entrances
    Former-commit-id: 6213ad3f841613591fb2fe995a9d03c8eb014e2a

---
+++ python/things/dungeon_snow.py
@@ -0,0 +1,44 @@
+import tp
+
+
+def thing_init(t):
+    return
+
+
+total_dungeon_snow = 0
+
+
+def dungeon_snow1_init(d1000_roll, tiles=[]):
+
+    global total_dungeon_snow
+
+    short_name = name = "dungeon_snow" + str(total_dungeon_snow + 1)
+    total_dungeon_snow = total_dungeon_snow + 1
+
+    x = tp.Tp(name=name,
+              is_dungeon_snow=True,
+              d1000_appearing_roll=d1000_roll)
+
+    x.set_long_name(short_name)
+    x.set_short_name(short_name)
+    x.set_is_dungeon(True)
+    x.set_z_depth("wall")
+    x.set_is_shadow_caster_soft(True)
+    x.set_is_shadow_caster(True)
+
+    for t in tiles:
+        x.set_tile(t, delay_ms=50)
+
+    x.thing_init = thing_init
+
+    return x
+
+
+def init():
+    # high numbers means rare, low means common
+    x = dungeon_snow1_init(d1000_roll=1, tiles=["dungeon_snow1"])
+    x.set_blit_top_off(2)
+    x.set_blit_left_off(1)
+    x.set_blit_right_off(1)
+
+init()
