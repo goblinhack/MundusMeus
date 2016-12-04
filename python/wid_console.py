17a78f904e98dbafbf15d43356fcdc548bdf748d
Neil McGill <goblinhack@gmail.com>
2016-12-04 10:40:11 -0500

    console
    Former-commit-id: 7a941d941974d14a1cf893084a21e190b9aefa07

---
+++ python/wid_console.py
@@ -0,0 +1,24 @@
+import wid_popup
+
+
+wid_console = None
+
+
+def create():
+    global wid_console
+
+    if wid_console is None:
+        w = wid_popup.WidPopup(name="test",
+                               x1=0.0, y1=0.0)
+        wid_console = w
+
+        w.add_text(text="test1")
+        w.add_text(text="test2")
+        w.add_text(text="test3")
+        w.set_color(bg=True, tl=True, br=True, name="red", alpha=0)
+        w.update()
+
+
+def destroy():
+    if wid_console is not None:
+        wid_console.destroy()
